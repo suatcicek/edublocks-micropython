694ed95704c21ef5771ac9f2890fefc320f81a34
suatcicek <45385686+suatcicek@users.noreply.github.com>
2018-11-27 12:42:41 +0300

    Update definitions.ts

--- src/blocks/donguler/definitions.ts
+++ src/blocks/donguler/definitions.ts
@@ -1,4 +1,26 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
- 
+ Blockly.Blocks['for'] = {
+  init: function() {
+    this.appendStatementInput("NAME")
+        .setCheck("String")
+        .appendField(new Blockly.FieldTextInput("... kere"), "forsart")
+        .appendField("tekrarla");
+    this.setColour(60);
+ this.setTooltip("deneme");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['while'] = {
+  init: function() {
+    this.appendStatementInput("NAME")
+        .setCheck(null)
+        .appendField(new Blockly.FieldTextInput("... olana kadar"), "sart")
+        .appendField("dön");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
  
 }
