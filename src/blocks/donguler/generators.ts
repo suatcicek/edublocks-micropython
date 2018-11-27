0f38ddf358b6d771195968efb1dee6608cae899a
suatcicek <45385686+suatcicek@users.noreply.github.com>
2018-11-27 12:43:57 +0300

    Update generators.ts

--- src/blocks/donguler/generators.ts
+++ src/blocks/donguler/generators.ts
@@ -1,4 +1,18 @@
 export default function define(Python: Blockly.BlockGenerators) {
- 
+ Blockly.Python['for'] = function(block) {
+  var text_forsart = block.getFieldValue('forsart');
+  var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['while'] = function(block) {
+  var text_sart = block.getFieldValue('sart');
+  var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
  
 }
